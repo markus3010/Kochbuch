9e6f4584a2e134aac281f60dfb396111b9e12edb
Markus <gahrma@t-online.de>
2018-04-26 11:01:12 +0200

    Aded StartSeiteView and itegrated it in ÜbersichtPage

---
+++ Kochbuch/Kochbuch/Views/StartSeiteView.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+using Xamarin.Forms;
+
+namespace Kochbuch
+{
+	public class StartSeiteView : ContentView
+	{
+        private ScrollView scroll;
+        private StackLayout layout;
+        private RezeptModel rezept;
+        public StartSeiteView(RezeptModel rezept)
+        {
+            this.rezept = rezept;
+            scroll = new ScrollView();
+            this.Content = scroll;
+            layout = new StackLayout();
+
+            scroll.Content = layout;
+
+
+            Image logo = new Image();
+            logo.Source = ImageSource.FromFile("Mensch.png");
+
+            Label willkommenLabel = new Label();
+            willkommenLabel.Text = "MeiKochbuch! - Ihr Onlinekochbuch!";
+            willkommenLabel.FontSize = Device.GetNamedSize(NamedSize.Large, typeof(Label));
+            willkommenLabel.FontAttributes = FontAttributes.Bold;
+            willkommenLabel.HorizontalOptions = LayoutOptions.CenterAndExpand;
+
+            Label tagLabel = new Label();
+            tagLabel.Text = "Rezedesages";
+            tagLabel.FontSize = Device.GetNamedSize(NamedSize.Large, typeof(Label));
+            //tagLabel.FontAttributes = FontAttributes.Bold;
+            tagLabel.HorizontalOptions = LayoutOptions.Start;
+
+
+
+            RezeptView rezeptView = new RezeptView(rezept);
+
+            rezeptView.Padding = new Thickness(20, 20, 20, 20);
+
+            layout.Children.Add(logo);
+            layout.Children.Add(willkommenLabel);
+            layout.Children.Add(tagLabel);
+            layout.Children.Add(rezeptView);
+        }
+
+        public void SetzeRezept(RezeptModel rezept)
+        {
+            this.rezept = rezept;
+        }
+    }
+}
--- Kochbuch/Kochbuch/ÜbersichtPage.cs
+++ Kochbuch/Kochbuch/ÜbersichtPage.cs
@@ -20,69 +20,52 @@ namespace Kochbuch
         {
 
 
+            RezeptModel rezept =
+               new RezeptModel
+               {
+                   Titel = "Schnitzel",
+                   Autor = "Markus Gahr",
+                   Schwierigkeit = RezeptModel.schwierigkeit.Leicht,
+                   Zutaten = new List<ZutatModel>
+                       {
+                            new ZutatModel
+                            {
+                                menge = 4,
+                                einheit = "",
+                                art = "Kalbsschnitzel"
+                            },
+                            new ZutatModel
+                            {
+                                menge = 2,
+                                einheit = "",
+                                art = "Eier"
+                            },
+                            new ZutatModel
+                            {
+                                menge = 2,
+                                einheit = "EL",
+                                art = "geschlangene Sahne"
+                            }
+                       },
+                   Beschreibung = @"Fleisch nacheinander in einem großen Gefrierbeutel ca. 5 mm dünn plattieren.
+    Eier und geschlagene Sahne in einer Schale mit der Gabel verquirlen. Die Sahne am besten schlagen, weil die Panierung dadurch luftiger wird.
+    Mehl und Semmelbrösel in 2 Schalen geben. Schnitzel rundum leicht mit
+     Salz würzen und nacheinander kurz in Mehl wenden. Die gesamte Oberfläche sollte gleichmäßig bemehlt sein, damit die Panierung gut hält. Überschüssiges Mehl abklopfen.
+    Schnitzel durch die Eiersahne ziehen und sofort in den Semmelbröseln wenden. Die Brösel nur leicht andrücken, nicht anpressen!
+    Schnitzel sofort in reichlich heißem (170 Grad) Butterschmalz in einer großen Pfanne auf jeder Seite ca. 3 Minuten unter gelegentlichem schwenken goldgelb ausbacken.
+    Schnitzel auf Küchenpapier abtropfen lassen und mit Petersilienkartoffeln, Gurkensalat (siehe Rezept: Gurkensalat) und Zitronenspalten servieren.
+    Warm halten bekommt Schnitzeln nicht. Am besten backen Sie sie frisch, teilen und backen dann die Nächsten."
+
+               };
+
+
+
             layout = new StackLayout();
             layout.VerticalOptions = LayoutOptions.FillAndExpand;
             layout.HorizontalOptions = LayoutOptions.FillAndExpand;
             this.Content = layout;
-
-            Label willkommenstext = new Label();
-            willkommenstext.HorizontalOptions = LayoutOptions.Start;
-            willkommenstext.VerticalOptions = LayoutOptions.CenterAndExpand;
-            willkommenstext.FontSize= Device.GetNamedSize(NamedSize.Large, typeof(Label));
-            willkommenstext.Text = "Etwas Neues für ihre Küche!";
-
-            layout.Children.Add(willkommenstext);
-            /*
-            //layout.BackgroundColor = Color.Aqua;
-           
+            layout.Children.Add(new StartSeiteView(rezept));
             
-
-            //leiste = Seitenleiste.getInstance(Width,Height);
-            //layout.Children.Add(leiste);
-            
-
-
-            List<RezeptModel> rezepte = new List<RezeptModel>
-            {
-                new RezeptModel
-                {
-                    titel = "Schnitzel",
-                    autor = "Markus Gahr",
-                    schwierigkeit = RezeptModel.Schwierigkeit.Leicht,
-                    zutaten  = new List<ZutatModel>
-                    {
-                        new ZutatModel
-                        {
-                            menge = 4,
-                            einheit = "",
-                            art = "Kalbsschnitzel"
-                        },
-                        new ZutatModel
-                        {
-                            menge = 2,
-                            einheit = "",
-                            art = "Eier"
-                        },
-                        new ZutatModel
-                        {
-                            menge = 2,
-                            einheit = "EL",
-                            art = "geschlangene Sahne"
-                        }
-                    },
-                    beschreibung = @"Fleisch nacheinander in einem großen Gefrierbeutel ca. 5 mm dünn plattieren.
-Eier und geschlagene Sahne in einer Schale mit der Gabel verquirlen. Die Sahne am besten schlagen, weil die Panierung dadurch luftiger wird.
-Mehl und Semmelbrösel in 2 Schalen geben. Schnitzel rundum leicht mit
- Salz würzen und nacheinander kurz in Mehl wenden. Die gesamte Oberfläche sollte gleichmäßig bemehlt sein, damit die Panierung gut hält. Überschüssiges Mehl abklopfen.
-Schnitzel durch die Eiersahne ziehen und sofort in den Semmelbröseln wenden. Die Brösel nur leicht andrücken, nicht anpressen!
-Schnitzel sofort in reichlich heißem (170 Grad) Butterschmalz in einer großen Pfanne auf jeder Seite ca. 3 Minuten unter gelegentlichem schwenken goldgelb ausbacken.
-Schnitzel auf Küchenpapier abtropfen lassen und mit Petersilienkartoffeln, Gurkensalat (siehe Rezept: Gurkensalat) und Zitronenspalten servieren.
-Warm halten bekommt Schnitzeln nicht. Am besten backen Sie sie frisch, teilen und backen dann die Nächsten."
-                }
-            };
-            SetRezepte(rezepte);
-            //layout.Children.Add(new RezeptErstellenView());
-            */
             this.SizeChanged += ÜbersichtPage_SizeChanged;
 
         }
